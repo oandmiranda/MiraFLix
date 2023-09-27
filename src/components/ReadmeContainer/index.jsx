2b4075c84c1a6ab19ef892633aa8f2d95a3416e9
Anderson Miranda <and.miranda1818@gmail.com>
2023-09-27 10:02:41 -0300

    componente ReadmeContainer refatorado

--- src/components/ReadmeContainer/index.jsx
+++ src/components/ReadmeContainer/index.jsx
@@ -15,7 +15,7 @@ export default function ReadmeContainer() {
                 <p>
                     Esta aplicação tem o front inspirado no streaming e organiza os pseudo-filmes em duas páginas! Uma é a Home com todos os vídeos listados e a outra está dividida por categorias. Cada vídeo tem o seu próprio ID que é extraído de um JSON mocado!
                     Os itens são filtrados pelo ID e posteriormente mapeados para serem organizados em um container de categoria. <br />
-                    Cada categoria está organizada pela lib de carrossel que exibe os cards já filtrados e organizados. Após o clique no Card o usuário é encaminhado para uma página "/watch/id" com o vídeo linkado ao YouTube simulando o streaming.
+                    Cada categoria está organizada pela lib de carrossel que exibe os cards já filtrados. Após o clique no Card o usuário é encaminhado para uma página "/watch/id" com o vídeo linkado ao YouTube simulando o streaming.
                 </p>
             </div>
 
